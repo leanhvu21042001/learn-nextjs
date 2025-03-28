dd402f12d29718261a59b3554397dc5745ea77a6
Lê Anh Vũ <61783362+leanhvu21042001@users.noreply.github.com>
2025-03-28 15:43:29 +0700

    Revert "fix: styles"
    This reverts commit 21a95d878a4e764b514833247f5be611722d7c92.

---
+++ src/components/Menu.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Fragment, useEffect, useState } from "react";
+// import styles from "./Menu.module.css";
+import { usePathname } from "next/navigation";
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
+  return (
+    // <nav className={styles.menuContainer}>
+    //   <div className={styles.menuLogo}>
+    //     <span>MyApp</span>
+    //   </div>
+
+    //   <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+    //     <span></span>
+    //     <span></span>
+    //     <span></span>
+    //   </div>
+
+    //   <ul className={`${styles.menuItems} ${isOpen ? styles.menuOpen : ""}`}>
+    //     <MenuItem label="Home" link="/" active={pathname === "/"} />
+    //     <MenuItem
+    //       label="App Todo"
+    //       link="/app-todo"
+    //       active={pathname === "/app-todo"}
+    //     />
+    //     <MenuItem
+    //       label="Page Todo"
+    //       link="/todo"
+    //       active={pathname === "/todo"}
+    //     />
+    //     <MenuItem
+    //       label="App Blog"
+    //       link="/app-blog"
+    //       active={pathname === "/app-blog"}
+    //     />
+    //     <MenuItem
+    //       label="Page Blog"
+    //       link="/blog"
+    //       active={pathname === "/blog"}
+    //     />
+    //     <MenuItem label="About" link="/about" active={pathname === "/about"} />
+    //   </ul>
+    // </nav>
+    <Fragment>
+      <div>
+        <h1>Menu</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Menu;
